1467d989bc5c3f1368858da5c0e914df3ea2e169
rainbowdango <minhkhoi1026@gmail.com>
2021-07-10 23:20:48 +0700

    add class elem for easy to understand

--- my_encrypt.py
+++ my_encrypt.py
@@ -1,12 +1,18 @@
 import numpy as np
 
+class elem:
+    def __init__(self, char, p, code = None):
+        self.char = char
+        self.p = p
+        self.code = code
+
 '''Calculate entropy of distribution p: H(p) = -(p_1 * log(p_1) + ... p_n * log(p_n))'''
 def entropy(p):
     res = 0
     n = len(p) # number of element in p
 
     for i in range(n):
-        res -= p[i] * np.log2(p[i])
+        res -= p[i].p * np.log2(p[i].p)
 
     return res
 
@@ -16,23 +22,25 @@ def cross_entropy(p, q):
     n = len(p) # number of element in p (or q)
 
     for i in range(n):
-        res -= p[i] * np.log2(q[i])
+        res -= p[i].p * np.log2(q[i].p)
 
     return res
 
-def mean_codeword_length(p, l):
+def mean_codeword_length(p):
     res = 0
     n = len(p) # number of element in p (or q)
 
     for i in range(n):
-        res += p[i] * l[i]
+        res += p[i].p * len(p[i].code)
 
     return res
 
+#def kraft_code(p):
+
+
 if __name__ == '__main__':
-    p = [0.5, 0.3, 0.2]
-    q = [0.4, 0.3, 0.3]
-    l = [1, 2, 3]
+    p = [elem('a', 0.5, "0"), elem('b', 0.3, "10"), elem('c', 0.2, "110")]
+    q = [elem('a', 0.4), elem('b', 0.3), elem('c', 0.3)]
     print(f"Entropy: {entropy(p)}")
     print(f"Cross entropy: {cross_entropy(p, q)}")
-    print(f"Mean codeword length: {mean_codeword_length(p, l)}")
+    print(f"Mean codeword length: {mean_codeword_length(p)}")
